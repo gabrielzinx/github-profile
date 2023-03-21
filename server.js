5ddec8269dfecefa79012512a043a461c1be31b6
gabrielzinx <gabriel.soaressc58@gmail.com>
2023-03-20 20:01:15 -0400

    serving API and web-page from the server

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const fs = require("fs");
 const port = 5000;
 
 const server = http.createServer((req, res) => {
-    res.end("Hello, World!");
+    const file = req.url === "/" ? "index.html" : req.url;
+    const filePath = path.join(__dirname, "public", file);
+    const extname = path.extname(filePath);
+
+    const allowedFileTypes = ['.html', '.css', '.js'];
+    const allowed = allowedFileTypes.find(item => item == extname);
+
+    if(!allowed) return;
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) throw err;
+        res.end(data);
+    });
 });
 
 server.listen(port, console.log(`Server is running: http://localhost:${port}`));
